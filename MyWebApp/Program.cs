21a66b009145b6f3f315523d2d1347640ee4af1f
Jacob Marks <jacobjmarks@outlook.com>
2024-04-15 09:18:48 +1000

    Add Mud

--- MyWebApp/Program.cs
+++ MyWebApp/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Options;
+using MudBlazor.Services;
 using MyWebApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
 
 builder.Services.Configure<AppConfig>(builder.Configuration.Bind);
 builder.Services.AddScoped<AppConfig>(services => services.GetRequiredService<IOptions<AppConfig>>().Value);
