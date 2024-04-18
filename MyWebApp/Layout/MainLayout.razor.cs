2b2b49aef9e55414b26f98a5932a7025aa3e1bf6
Jacob Marks <jacobjmarks@outlook.com>
2024-04-18 21:16:53 +1000

    Separate code from views

---
+++ MyWebApp/Layout/MainLayout.razor.cs
@@ -0,0 +1,42 @@
+﻿using MudBlazor;
+
+namespace MyWebApp.Layout;
+
+public partial class MainLayout
+{
+    private string _darkLightModeText = null!;
+    private string _darkLightModeIcon = null!;
+    private bool _isDarkMode;
+
+    private MudThemeProvider _mudThemeProvider = null!;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _isDarkMode = await _mudThemeProvider.GetSystemPreference();
+            UpdateDarkLightModeIcon();
+            StateHasChanged();
+        }
+    }
+
+    private void ToggleDarkLightMode()
+    {
+        _isDarkMode = !_isDarkMode;
+        UpdateDarkLightModeIcon();
+    }
+
+    private void UpdateDarkLightModeIcon()
+    {
+        if (_isDarkMode)
+        {
+            _darkLightModeText = "Switch to Light Mode";
+            _darkLightModeIcon = Icons.Material.Filled.LightMode;
+        }
+        else
+        {
+            _darkLightModeText = "Switch to Dark Mode";
+            _darkLightModeIcon = Icons.Material.Filled.DarkMode;
+        }
+    }
+}
---
+++ MyWebApp/Pages/Home.razor.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net.Http.Json;
+using MySharedClassLib;
+using Microsoft.AspNetCore.Components;
+
+namespace MyWebApp.Pages;
+
+public partial class Home : IDisposable
+{
+    [Inject]
+    private AppConfig AppConfig { get; set; } = null!;
+
+    private string? CurrentInputValue { get; set; }
+
+    private readonly HttpClient httpClient = new();
+    private readonly TimeSpan minRequestInterval = TimeSpan.FromSeconds(1);
+    private DateTime lastRequestAt = DateTime.MinValue;
+    private string? lastRequestedInputValue;
+
+    private bool isLoading = false;
+
+    private IEnumerable<BindingResult> bindingResults = [];
+    private bool showErroneous = true;
+
+    private async Task InputOnChange()
+    {
+        if (CurrentInputValue == null)
+            return;
+
+        isLoading = true;
+
+        try
+        {
+            if (DateTime.Now - lastRequestAt < minRequestInterval)
+            {
+                if (CurrentInputValue != lastRequestedInputValue)
+                {
+                    await Task.Delay(50);
+                    await InputOnChange();
+                }
+
+                return;
+            }
+
+            lastRequestAt = DateTime.Now;
+            await GetBindingResultsAsync(CurrentInputValue);
+        }
+        finally
+        {
+            isLoading = false;
+        }
+    }
+
+    private async Task GetBindingResultsAsync(string inputValue)
+    {
+        lastRequestedInputValue = inputValue;
+        var qs = QueryString.Create("q", "?q=" + inputValue);
+        using var response = await httpClient.GetAsync(AppConfig.AzureFunctionUrl + qs);
+        response.EnsureSuccessStatusCode();
+        var results = await response.Content.ReadFromJsonAsync<IEnumerable<BindingResult>>() ?? [];
+        bindingResults = results.OrderByDescending(r => !r.IsErroneous);
+    }
+
+    public void Dispose()
+    {
+        httpClient.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
