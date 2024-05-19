889286cdcf84c1affcacb9f6355521dd9e8f0acb
Jacob Marks <jacobjmarks@outlook.com>
2024-05-19 20:38:27 +1000

    Remember theme

--- src/Client/Layout/MainLayout.razor.cs
+++ src/Client/Layout/MainLayout.razor.cs
@@ -1,20 +1,34 @@
-﻿using MudBlazor;
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Client.Layout;
 
 public partial class MainLayout
 {
-    private string _darkLightModeText = null!;
-    private string _darkLightModeIcon = null!;
-    private bool _isDarkMode;
+    [Inject] private LocalStorageService LocalStorageService { get; set; } = null!;
 
     private MudThemeProvider _mudThemeProvider = null!;
+
+    private bool _isDarkMode;
+    private bool IsDarkMode
+    {
+        get => _isDarkMode;
+        set
+        {
+            _isDarkMode = value;
+            LocalStorageService.SetItemAsync("qsd:cfg:dark-mode", value).AndForget();
+        }
+    }
+
+    private string _darkLightModeText = null!;
+    private string _darkLightModeIcon = null!;
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         if (firstRender)
         {
-            _isDarkMode = await _mudThemeProvider.GetSystemPreference();
+            var themePreference = await LocalStorageService.GetItemAsync<bool?>("qsd:cfg:dark-mode");
+            IsDarkMode = themePreference ?? await _mudThemeProvider.GetSystemPreference();
             UpdateDarkLightModeIcon();
             StateHasChanged();
         }
@@ -22,13 +36,13 @@ public partial class MainLayout
 
     private void ToggleDarkLightMode()
     {
-        _isDarkMode = !_isDarkMode;
+        IsDarkMode = !IsDarkMode;
         UpdateDarkLightModeIcon();
     }
 
     private void UpdateDarkLightModeIcon()
     {
-        if (_isDarkMode)
+        if (IsDarkMode)
         {
             _darkLightModeText = "Switch to Light Mode";
             _darkLightModeIcon = Icons.Material.Filled.LightMode;
